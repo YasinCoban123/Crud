0ab16264396c6fd29674f595f831a603b940c540
YasinCoban123 <9018681@student.zadkine.nl>
2023-05-11 11:26:28 +0100

    rr

---
+++ navbar.php
@@ -0,0 +1,14 @@
+
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+<nav class="nav">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="crud_bieren.php">CRUD bieren</a></li>
+    </ul>
+  </nav>
+  </body>
+  </html>
